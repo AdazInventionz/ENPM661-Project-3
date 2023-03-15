570b165e21527f3b6067a24978d31e1aafd3d6ae
AdazInventionz <56547728+AdazInventionz@users.noreply.github.com>
2023-03-15 14:49:27 -0400

    Check Obstacle Border
    Added a function that accepts an x and y value and returns "True" if that point lies within the 5 pixel border range of an obstacle and "False" if it does not; Generated arena now also includes borders (in gray)

--- Project 3.py
+++ Project 3.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[29]:
+# In[54]:
 
 
 #Imported libraries
 import numpy as np
 import cv2 as cv
 import time
+import math
 
 #Sets up arena with obstacles
 def setup():
@@ -18,15 +19,23 @@ def setup():
     white = (255, 255, 255)
     gray = (177, 177, 177)
     
+    #Draw Obstacle Borders
+    for x in range(0, 600):
+
+        for y in range(0, 250):
+        
+            if checkBorder(x, y):
+                arena[y, x] = gray
+    
     #Draw Obstacles
     for x in range(0, 600):
 
         for y in range(0, 250):
         
             if checkObstacle(x, y):
-                arena[y, x] = (255, 255, 255)
+                arena[y, x] = white
                 
-#Checks to see if a node is an obstacle (or its border)
+#Checks to see if a point is within an obstacle
 def checkObstacle(x, y):
     
     #Both Rectangles
@@ -55,6 +64,38 @@ def checkObstacle(x, y):
         
     return False
   
+#Checks to see if a point is within the border of an obstacle
+def checkBorder(x, y):
+    
+    triHeight = int(round(5/math.cos(math.radians(63.4))))
+    hexHeight = int(round(5/math.cos(math.radians(30.3))))
+    
+    #Both Rectangles
+    if x >= 100 - 5 and x <= 150 + 5:
+        
+        if y < 100 + 5 or y >= 150 - 5:
+            return True
+    
+    #Pentagon (Left Half)
+    if x >= 235 - 5 and x <= 300:
+        
+        if (y >= (-38/65)*x + (2930/13) - hexHeight) and (y <= (38/65)*x + (320/13) + hexHeight):
+            return True
+    
+    #Pentagon (Right Half)
+    if x >= 300 and x <= 366 + 5:
+        
+        if (y >= (38/65)*x + (-1630/13) - hexHeight) and (y <= (-38/65)*x + (4880/13) + hexHeight):
+            return True
+    
+    #Triangle
+    if x >= 460 - 5 and x <= 510 + 5:
+        
+        if (y >= 2*x - 895 - triHeight) and (y <= -2*x + 1145 + triHeight):
+            return True
+        
+    return False  
+    
     
     
     
@@ -66,6 +107,12 @@ setup()
 cv.imshow("Arena", arena)
 cv.waitKey()
 cv.destroyAllWindows()
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
