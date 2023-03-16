bb71aa9ff28c4ad164d5bb508c056c60505fd440
AdazInventionz <56547728+AdazInventionz@users.noreply.github.com>
2023-03-15 23:09:46 -0400

    Check Clearance
    Added a function that accepts an x and y value and returns "True" if that point lies within the a clearance envelope (determined by the radius of the bot) and "False" if it does not; Generated arena now also includes clearance envelopes (in dark gray); Also modified to remove excessive borders/clearance envelopes from top and bottom tips of triangle obstacle; Also added a function checkValid, which accepts an x and y value as well as the radius (r) of the robot and returns "True" if the point is not within an obstacle, border, or clearance envelope and the point is within the bounds of the arena and returns "False" if any of these conditions are not met

--- Project 3.py
+++ Project 3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[54]:
+# In[71]:
 
 
 #Imported libraries
@@ -9,6 +9,10 @@ import numpy as np
 import cv2 as cv
 import time
 import math
+
+
+
+
 
 #Sets up arena with obstacles
 def setup():
@@ -18,6 +22,15 @@ def setup():
     #Colors
     white = (255, 255, 255)
     gray = (177, 177, 177)
+    darkGray = (104, 104, 104)
+    
+    #Draw Radial Clearance
+    for x in range(0, 600):
+
+        for y in range(0, 250):
+        
+            if checkClearance(x, y, 2):
+                arena[y, x] = darkGray
     
     #Draw Obstacle Borders
     for x in range(0, 600):
@@ -91,15 +104,69 @@ def checkBorder(x, y):
     #Triangle
     if x >= 460 - 5 and x <= 510 + 5:
         
-        if (y >= 2*x - 895 - triHeight) and (y <= -2*x + 1145 + triHeight):
+        if (y >= 2*x - 895 - triHeight) and (y <= -2*x + 1145 + triHeight) and (y >= 25 - 5) and (y <= 225 + 5):
             return True
         
-    return False  
+    return False
+
+#Checks to see if a point is within radial clearance of a border
+def checkClearance(x, y, r):
     
+    rr = r - 1
     
+    if rr == 0:
+        return False
     
+    triHeight = int(round((5 + rr)/math.cos(math.radians(63.4))))
+    hexHeight = int(round((5 + rr)/math.cos(math.radians(30.3))))
     
+    #Both Rectangles
+    if x >= 100 - 5 - rr and x <= 150 + 5 + rr:
+        
+        if y < 100 + 5 + rr or y >= 150 - 5 - rr:
+            return True
     
+    #Pentagon (Left Half)
+    if x >= 235 - 5 - rr and x <= 300:
+        
+        if (y >= (-38/65)*x + (2930/13) - hexHeight) and (y <= (38/65)*x + (320/13) + hexHeight):
+            return True
+    
+    #Pentagon (Right Half)
+    if x >= 300 and x <= 366 + 5 + rr:
+        
+        if (y >= (38/65)*x + (-1630/13) - hexHeight) and (y <= (-38/65)*x + (4880/13) + hexHeight):
+            return True
+    
+    #Triangle
+    if x >= 460 - 5 - rr and x <= 510 + 5 + rr:
+        
+        if (y >= 2*x - 895 - triHeight) and (y <= -2*x + 1145 + triHeight) and (y >= 25 - 5 - rr) and (y <= 225 + 5 + rr):
+            return True
+        
+    return False
+
+#Checks to see if a point is valid (by checking obstacle, border, and clearance, as well as making sure the point is within arena bounds)
+def checkValid(x, y, r):
+    
+    if checkObstacle(x, y):
+        return False
+    
+    if checkBorder(x, y):
+        return False
+    
+    if checkClearance(x, y, r):
+        return False
+    
+    if (x < 0 or x >= 600 or y < 0 or y >= 250):
+        return False
+    
+    return True
+
+
+
+
+
 #Main Code
 arena = np.zeros((250, 600, 3), dtype = "uint8")
         
@@ -107,12 +174,6 @@ setup()
 cv.imshow("Arena", arena)
 cv.waitKey()
 cv.destroyAllWindows()
-
-
-# In[ ]:
-
-
-
 
 
 # In[ ]:
